692bec564b3f2644a365d5912cbc905248ece191
Fipsi <phigattenloehner@outlook.com>
2024-10-23 15:21:41 +0200

    add bash script for k8s cmds

--- k8s_script.sh
+++ k8s_script.sh
@@ -1,1 +1,14 @@
 #!bin/bash
+echo "Starting k8s script!"
+
+echo kubectl top nodes
+
+echo "Please choose your namespace: " 
+
+echo kubectl get namespaces
+
+echo kubectl get pods -n $namespace -o wide
+
+echo kubectl describe pod $pod -n $namespace
+
+echo kubectl top pods -n $namespace
